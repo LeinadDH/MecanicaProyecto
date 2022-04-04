4d936c020c0249a46754b9c093b89a0be84caee7
Daniel Navarrete <daniel_navarrete_05@hotmail.com>
2022-04-04 03:05:17 -0500

    World Gravity
    Funcional sin triggers

--- Assets/Code/Input/MoveInput.cs
+++ Assets/Code/Input/MoveInput.cs
@@ -9,63 +9,66 @@ public class MoveInput : MovementInputManager
 
     public Vector2 moveDirection;
     public float jumpSpeed = 10f;
-    public float gravity = -0.010f;
+    public float Gravity = 0;
     public float maxAceletarion;
 
     public bool jump = false;
     public bool isGrounded = false;
 
     public float LastX;
+    public Vector2 vectorD;
+    public Vector2 gravityV;
+
+    public GameObject planet;
 
     public void Update()
     {
         Debug.DrawRay(transform.position, Movement, Color.red);
 
-        //Usando Fixed time (movimiento más suave)
-        //transform.Translate(new Vector3(Movement.x, 0f, 0f) * movementSpeed * Time.fixedDeltaTime);
-        //Sin fixed time (movimiento más incosistente)
-        transform.Translate(new Vector3(Movement.x, 0f, 0f) * movementSpeed * Time.deltaTime);
+        moveDirection.x = Movement.x * movementSpeed * Time.fixedDeltaTime;
+
+        vectorD = this.transform.position - planet.transform.position;
+
+        gravityV = vectorD.normalized - moveDirection;
 
         AuthenticBoxColider();
 
-        sideCollitions();
+        //sideCollitions();
+
+        Vector2 distanceVector = (Vector2)planet.transform.position - (Vector2)transform.position;
+        float angle = Mathf.Atan2(distanceVector.y, distanceVector.x) * Mathf.Rad2Deg;
+        transform.localRotation = Quaternion.AngleAxis(angle + 90, Vector3.forward);
 
         if (isGrounded == true)
         {
             if (jump == true)
             {
-                gravity = -0.1f;
-                //Usando Fixed time (movimiento más suave)
-                //moveDirection.y = jumpSpeed * Time.fixedDeltaTime * 100;
-                //Sin fixed time (movimiento más incosistente)
-                moveDirection.y = jumpSpeed * Time.deltaTime * 100;
-                if (moveDirection.y > maxAceletarion)
-                {
-                    isGrounded = false;
-                }
+                moveDirection.y = jumpSpeed * Time.fixedDeltaTime * 100;
+                isGrounded = false; 
             }
-            else if(jump == false)
+            else if (jump == false)
             {
-                moveDirection.y = 0.0f;
-                gravity = -0.3f;
+                moveDirection.y = 0f;
             }
+
         }
-        else if(isGrounded == false)
+        else if (isGrounded == false)
         {
-            if(jump == false)
+            Gravity = Mathf.Abs(0.2f);
+            moveDirection.y -= Mathf.Abs(gravityV.y * Gravity * Time.fixedDeltaTime);
+
+            if (jump == false)
             {
-                gravity = -0.3f;
+                moveDirection.y -= Mathf.Abs(gravityV.y * Gravity * Time.fixedDeltaTime);
             }
-            if(jump == true)
+            if (jump == true)
             {
-                gravity = -0.1f;
+                StartCoroutine(StopJump());
             }
-            //Usando Fixed time (movimiento más suave)
-            //moveDirection.y += gravity * Time.fixedDeltaTime;
-            //Sin fixed time (movimiento más incosistente)
-            moveDirection.y += gravity * Time.deltaTime;
+            StartCoroutine(StopJump());
+
         }
-        
+
         transform.Translate(moveDirection);
     }
 
@@ -86,37 +89,39 @@ public class MoveInput : MovementInputManager
 
     public void AuthenticBoxColider()
     {
-        if(Physics2D.OverlapBox(new Vector2(transform.position.x, transform.position.y), new Vector2(1f, 1f), 0f))
+        if (Physics2D.OverlapBox(new Vector2(transform.position.x, transform.position.y), new Vector2(1f, 1f), 0f))
         {
             isGrounded = true;
+            moveDirection.y = 0f;
         }
-        else
+        if (Physics2D.OverlapBox(new Vector2(transform.position.x, transform.position.y), new Vector2(1f, 1f), 0f) == null)
         {
             isGrounded = false;
         }
-    }
 
+    }
+    
     public void sideCollitions()
     {
         RaycastHit2D hit = Physics2D.Raycast(transform.position, Movement, 0.5f);
 
-        if(hit.collider != null)
+        if (hit.collider != null)
         {
             LastX = Movement.x;
             Movement.x += Movement.x * -2;
-            StartCoroutine(StopMove());
         }
     }
 
     void OnDrawGizmos()
     {
         Gizmos.color = Color.red;
-        Gizmos.DrawWireCube(new Vector2(transform.position.x, transform.position.y), new Vector2(1f, 1f));
     }
 
-    IEnumerator StopMove()
+
+    IEnumerator StopJump()
     {
-        yield return new WaitForSeconds(0.15f);
-        Movement.x = 0;
+        yield return new WaitForSeconds(0.1f);
+        jump = false;
+        isGrounded = false;
     }
 }
